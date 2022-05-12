8a453b3cdd493846e15aeb342dae8252ee32dfc2
Aleksandra V <vab.work@gmail.com>
2022-05-12 14:53:59 -0700

    3 commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+
 import './index.css';
 import {App} from './components/App/App';
 import { Provider } from "react-redux";
@@ -9,9 +9,7 @@ import { store } from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </Provider>
 );
 
